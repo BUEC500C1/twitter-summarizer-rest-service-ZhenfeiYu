4ae3741fdcbefdf2785436f6e2ebdd97028dc53e
ZhenfeiYu <55257370+ZhenfeiYu@users.noreply.github.com>
2020-04-06 03:30:24 -0400

    Update get_video.py

--- get_video.py
+++ get_video.py
@@ -9,7 +9,7 @@ def tweet2video(username,searchword):
     for root,dirs,files in os.walk(file_dir):
         for file in files:
            list.append(file)
-    video=cv2.VideoWriter('/myVideo'+username+'.avi',cv2.VideoWriter_fourcc(*'MJPG'),0.5,(1280,720))  
+    video=cv2.VideoWriter('../'+username+'/'+username+'.avi',cv2.VideoWriter_fourcc(*'MJPG'),0.5,(1280,720))   
     for i in range(1,len(list)+1):
         img=cv2.imread(file_dir+list[i-1])  #read the images
         img=cv2.resize(img,(1280,720)) #transfer images to 1280*720
@@ -17,7 +17,7 @@ def tweet2video(username,searchword):
     video.release()
 
 if __name__ == '__main__':
-   tweet2video(sys.argv[1],sys.argv[2])
-#     foldername = input('Enter your foldername: ')
-#     searchword = input('Enter the searchword you would like to search: ')
-#     tweet2video(foldername,searchword)
+    tweet2video(sys.argv[1],sys.argv[2])
+#    foldername = input('Enter your foldername: ')
+#    searchword = input('Enter the searchword you would like to search: ')
+#    tweet2video(foldername,searchword)
