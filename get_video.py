adf8a7d41fe97d37176bd01b61a92d5bbb622f1c
ZhenfeiYu <55257370+ZhenfeiYu@users.noreply.github.com>
2020-04-07 14:07:50 -0400

    Update get_video.py

--- get_video.py
+++ get_video.py
@@ -9,7 +9,7 @@ def tweet2video(username,searchword):
     for root,dirs,files in os.walk(file_dir):
         for file in files:
            list.append(file)
-    video=cv2.VideoWriter('../'+username+'/'+username+'.avi',cv2.VideoWriter_fourcc(*'MJPG'),0.5,(1280,720))   
+    video=cv2.VideoWriter(username+'/'+username+'.avi',cv2.VideoWriter_fourcc(*'MJPG'),0.5,(1280,720))   
     for i in range(1,len(list)+1):
         img=cv2.imread(file_dir+list[i-1])  #read the images
         img=cv2.resize(img,(1280,720)) #transfer images to 1280*720
