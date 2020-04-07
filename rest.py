fe5176ee31b4f0f52f12bd4196c6b761bcaece04
ZhenfeiYu <55257370+ZhenfeiYu@users.noreply.github.com>
2020-04-07 14:06:11 -0400

    Update rest.py

--- rest.py
+++ rest.py
@@ -8,24 +8,23 @@ application = Flask(__name__)
 @application.route('/') 
 def root():
     return render_template('main.html')
+
 @application.route('/', methods=['POST']) 
 def post():
     text1 = request.form['username'] 
-    # text2 = request.form['searchword'] 
-    args = "python3 queue_work.py"
+    text2 = request.form['searchword'] 
+    args = "python3 get_video.py"+" "+str(text1)+" "+str(text2)
     os.system(args)
-    zipFolder = zipfile.ZipFile('myVideo.zip','w', zipfile.ZIP_DEFLATED)
-    for root, directs, files in os.walk('User/yzf/hw5/myVideo/'):
-        for f in files:
-            print(f)
-            zipFolder.write('User/yzf/hw5/myVideo/' + str(f))
-    zipFolder.close()
-   
-    return send_file('myVideo.zip', mimetype ='zip', attachment_filename = 'myVideo.zip', as_attachment=True)
+    # zipFolder = zipfile.ZipFile('myVideo.zip','w', zipfile.ZIP_DEFLATED)
+    # for root, directs, files in os.walk(text1):
+    #     for f in files:
+    #         print(f)
+    return send_file(str(text1)+'/'+str(text1)+'.avi', attachment_filename = str(text1)+'.avi', as_attachment=True)
+            # zipFolder.write('myVideo/' + str(f))
+    # zipFolder.close()
+    
 
 if __name__ == '__main__':
 
-    application.run(host = '0.0.0.0', port = 8240)
-
-
+    application.run(host = '0.0.0.0', port = 5000)
 
